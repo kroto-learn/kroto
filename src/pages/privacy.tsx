5d9d416fe300cb02d04c06d1af56dbf91c69f287
rosekamallove <rosekamallove@gmail.com>
2023-04-05 15:59:14 +0530

    fixed: type errors

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -3,11 +3,11 @@ export default function PrivacyPage() {
     <div className="mx-auto my-20 w-11/12 sm:w-10/12 md:w-8/12 lg:w-6/12">
       <div className="flex flex-grow flex-col gap-3">
         <div className="flex min-h-[20px] flex-col items-start gap-4 whitespace-pre-wrap">
-          <div className="markdown prose prose-invert w-full break-words">
+          <div className="markdown prose prose-pink w-full break-words">
             <h1>Privacy Policy for Kroto</h1>
             <p>
-              Kroto Creator Labs ("us", "we", or "our") operates the kroto.in
-              website (the "Service").
+              {`Kroto Creator Labs ("us", "we", or "our") operates the kroto.in
+              website (the "Service").`}
             </p>
             <p>
               This page informs you of our policies regarding the collection,
@@ -27,10 +27,10 @@ export default function PrivacyPage() {
             <h3>Types of Data Collected</h3>
             <h4>Personal Data</h4>
             <p>
-              While using our Service, we may ask you to provide us with certain
+              {`While using our Service, we may ask you to provide us with certain
               personally identifiable information that can be used to contact or
               identify you ("Personal Data"). Personally identifiable
-              information may include, but is not limited to:
+              information may include, but is not limited to:`}
             </p>
             <ul>
               <li>Email address</li>
