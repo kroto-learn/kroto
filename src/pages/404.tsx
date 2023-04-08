0cff8721299d453d5cf5ffc9740ebddbbc4988b4
rosekamallove <rosekamallove@gmail.com>
2023-04-08 17:18:47 +0530

    added: loader && app in pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,18 +3,18 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <section className="h-screen flex items-center justify-center">
+    <section className="flex h-screen items-center justify-center">
       <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
         <div className="mx-auto max-w-screen-sm text-center">
           <h1 className="mb-4 text-7xl font-extrabold tracking-tight text-neutral-200 lg:text-9xl">
             404
           </h1>
           <p className="mb-4 text-3xl font-bold tracking-tight text-neutral-300 md:text-4xl">
-            Something's missing.
+            Something&apos;s missing.
           </p>
           <p className="mb-4 text-lg font-light text-neutral-400">
-            Sorry, we can't find that page. You'll find lots to explore on the
-            home page.{" "}
+            Sorry, we can&apos;t find that page. You&apos;ll find lots to
+            explore on the home page.{" "}
           </p>
           <Link
             href="/"
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,34 @@ import { SessionProvider } from "next-auth/react";
 
 import { api } from "@/utils/api";
 
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = () => NProgress.start();
+    const handleStop = () => NProgress.done();
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
+  }, [router]);
+
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
