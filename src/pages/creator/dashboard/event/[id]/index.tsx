3d3e9c6cbe881e0655af569702ebd4291541bb16
Yuvraj Singh <ysgaur9919@gmail.com>
2023-04-18 15:18:42 +0530

    share

--- src/pages/creator/dashboard/event/[id]/index.tsx
+++ src/pages/creator/dashboard/event/[id]/index.tsx
@@ -38,7 +38,7 @@ import {
   UserPlusIcon,
   XMarkIcon,
   RocketLaunchIcon,
-  ShareIcon,
+  LinkIcon,
 } from "@heroicons/react/20/solid";
 
 const EventOverview = () => {
@@ -64,6 +64,8 @@ const EventOverview = () => {
   const date = event && event.datetime ? new Date(event.datetime) : new Date();
   const [startEventModal, setStartEventModal] = useState(false);
 
+  const { successToast } = useToast();
+
   // const { data: creator } = api.creator.getProfile.useQuery();
 
   // useEffect(() => {
@@ -85,6 +87,25 @@ const EventOverview = () => {
         <Head>
           <title>{`${event?.title ?? "Event"} | Overview`}</title>
         </Head>
+        <div className="flex w-full items-center justify-between gap-4 rounded-xl bg-neutral-800 p-1 px-3">
+          <div className="flex items-center gap-2">
+            <span className="relative flex h-3 w-3 items-center justify-center">
+              <span className="absolute h-full w-full animate-ping rounded-full bg-pink-500 opacity-75"></span>
+              <span className="h-4/5 w-4/5 rounded-full bg-pink-500"></span>
+            </span>
+            The Event is Live Now.
+          </div>
+          {event.eventType === "virtual" ? (
+            <Link
+              href={event?.eventUrl ?? ""}
+              className={`group inline-flex items-center justify-center gap-2 rounded-xl bg-pink-500/20 px-4 py-2 text-center text-xs font-medium text-pink-600 transition-all duration-300 hover:bg-pink-500 hover:text-neutral-200`}
+            >
+              Join Event
+            </Link>
+          ) : (
+            <></>
+          )}
+        </div>
         <div className="flex w-full max-w-3xl flex-col justify-start gap-4 rounded-xl bg-neutral-800 p-4">
           <div className="flex w-full items-start gap-8">
             <div className="flex flex-col items-start gap-4">
@@ -145,7 +166,7 @@ const EventOverview = () => {
             </div>
           </div>
 
-          <div className="flex w-full items-center justify-between gap-8">
+          <div className="flex w-full items-center gap-12">
             <div className="flex items-center gap-4">
               <button
                 onClick={() => {
@@ -162,7 +183,7 @@ const EventOverview = () => {
                 onClick={() => {
                   setStartEventModal(true);
                 }}
-                className={`group inline-flex items-center justify-center gap-2 rounded-xl bg-pink-500/20 px-4 py-2 text-center text-xs font-medium text-pink-600 transition-all duration-300 hover:bg-pink-500 hover:text-neutral-200`}
+                className={`group inline-flex items-center justify-center gap-2 rounded-xl bg-pink-500/20 px-4 py-2 text-center text-xs font-medium text-pink-600  transition-all duration-300 hover:bg-pink-500 hover:text-neutral-200`}
               >
                 <RocketLaunchIcon className="w-3" />
                 Start Event
@@ -170,33 +191,57 @@ const EventOverview = () => {
             </div>
 
             <div className="flex items-center gap-2">
-              <ShareIcon className="w-5" />
-              <p>Share Event</p>
+              <button
+                className="aspect-square rounded-full bg-neutral-700 p-2 grayscale duration-300 hover:bg-neutral-600 hover:grayscale-0"
+                onClick={() => {
+                  void navigator.clipboard.writeText(
+                    `https://kroto.in/event/${event?.id ?? ""}`
+                  );
+                  successToast("Event URL copied to clipboard!");
+                }}
+              >
+                <LinkIcon className="w-3" />
+              </button>
               <LinkedinShareButton
                 url={`https://kroto.in/event/${event?.id ?? ""}`}
               >
-                <LinkedinIcon size={24} round />
+                <LinkedinIcon
+                  size={28}
+                  round
+                  className="grayscale duration-300 hover:grayscale-0"
+                />
               </LinkedinShareButton>
               <FacebookShareButton
-                className=""
                 url={`https://kroto.in/event/${event?.id ?? ""}`}
                 quote={`Join the "${event?.title ?? ""}" event on Kroto.in`}
                 hashtag={"#kroto"}
               >
-                <FacebookIcon size={24} round />
+                <FacebookIcon
+                  size={28}
+                  round
+                  className="grayscale duration-300 hover:grayscale-0"
+                />
               </FacebookShareButton>
               <TwitterShareButton
                 url={`https://kroto.in/event/${event?.id ?? ""}`}
                 title={`Join the "${event?.title ?? ""}" event on Kroto.in`}
               >
-                <TwitterIcon size={24} round />
+                <TwitterIcon
+                  size={28}
+                  round
+                  className="grayscale duration-300 hover:grayscale-0"
+                />
               </TwitterShareButton>
               <WhatsappShareButton
                 url={`https://kroto.in/event/${event?.id ?? ""}`}
                 title={`Join the "${event?.title ?? ""}" event on Kroto.in`}
-                separator=":: "
+                separator=": "
               >
-                <WhatsappIcon size={24} round />
+                <WhatsappIcon
+                  size={28}
+                  round
+                  className="grayscale duration-300 hover:grayscale-0"
+                />
               </WhatsappShareButton>
             </div>
           </div>
@@ -227,7 +272,7 @@ const EventOverview = () => {
               </div>
               {/* <!-- Modal body --> */}
               <div className="space-y-1 p-2">
-                <p className="px-4 text-base leading-relaxed text-neutral-500 dark:text-neutral-400">
+                <p className="px-4 text-base leading-relaxed text-neutral-300">
                   Are you sure you want to start the{" "}
                   <span className="font-medium">
                     &quot;{event?.title ?? ""}
@@ -236,39 +281,23 @@ const EventOverview = () => {
                   event now?
                 </p>
                 <br />
-                <p className="px-4 text-base leading-relaxed text-neutral-500 dark:text-neutral-400">
+                <p className="px-4 text-base leading-relaxed text-neutral-300">
                   A notification will be send to all the registered users.
                 </p>
               </div>
               {/* <!-- Modal footer --> */}
               <div className="flex items-center space-x-2 rounded-b p-4 text-sm dark:border-neutral-600">
                 <Link
-                  href={event?.eventUrl ?? ""}
+                  href={
+                    event?.eventType === "virtual" ? event?.eventUrl ?? "" : ":"
+                  }
+                  onClick={() => {
+                    setStartEventModal(false);
+                  }}
                   target="_blank"
                   type="button"
                   className="rounded-lg bg-pink-500/50 px-5 py-2.5 text-center text-sm font-medium text-neutral-200/70 duration-300 hover:bg-pink-500 hover:text-neutral-200"
                 >
-                  {/* {isLoading ? (
-                    <svg
-                      aria-hidden="true"
-                      role="status"
-                      className="mr-3 inline h-4 w-4 animate-spin text-white"
-                      viewBox="0 0 100 101"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                        fill="#E5E7EB"
-                      />
-                      <path
-                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                  ) : (
-                    <></>
-                  )}{" "} */}
                   Start Event
                 </Link>
                 <button
