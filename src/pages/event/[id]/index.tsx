d68d65587f6d532068ff839bced23732b3cf6dfc
Yuvraj Singh <ysgaur9919@gmail.com>
2023-05-04 15:53:44 +0530

    update: static og image for event

--- src/pages/event/[id]/index.tsx
+++ src/pages/event/[id]/index.tsx
@@ -68,6 +68,10 @@ export default function EventPage({ eventId }: Props) {
   const { mutateAsync: addToCalendarMutation, isLoading: addingToCalendar } =
     api.email.sendCalendarInvite.useMutation();
 
+  const ogImage = `https://kroto.s3.ap-south-1.amazonaws.com/og_event_${
+    event?.id ?? ""
+  }.png`;
+
   return (
     <Layout>
       <Head>
@@ -77,33 +81,12 @@ export default function EventPage({ eventId }: Props) {
         {/* Google SEO */}
         <meta itemProp="name" content={event?.title ?? ""} />
         <meta itemProp="description" content={event?.description ?? ""} />
-        <meta
-          itemProp="image"
-          content={`https://kroto.in/api/og/event?title=${
-            event?.title ?? ""
-          }&datetime=${event?.datetime?.getTime() ?? 0}&host=${
-            event?.creator?.name ?? ""
-          }`}
-        />
+        <meta itemProp="image" content={ogImage} />
         {/* Facebook meta */}
         <meta property="og:title" content={event?.title ?? ""} />
         <meta property="og:description" content={event?.description ?? ""} />
-        <meta
-          property="og:image"
-          content={`https://kroto.in/api/og/event?title=${
-            event?.title ?? ""
-          }&datetime=${event?.datetime?.getTime() ?? 0}&host=${
-            event?.creator?.name ?? ""
-          }`}
-        />
-        <meta
-          property="image"
-          content={`https://kroto.in/api/og/event?title=${
-            event?.title ?? ""
-          }&datetime=${event?.datetime?.getTime() ?? 0}&host=${
-            event?.creator?.name ?? ""
-          }`}
-        />
+        <meta property="og:image" content={ogImage} />
+        <meta property="image" content={ogImage} />
         <meta
           property="og:url"
           content={`https://kroto.in/event/${event?.id ?? ""}`}
@@ -112,14 +95,7 @@ export default function EventPage({ eventId }: Props) {
         {/* twitter meta */}
         <meta name="twitter:title" content={event?.title ?? ""} />
         <meta name="twitter:description" content={event?.description ?? ""} />
-        <meta
-          name="twitter:image"
-          content={`https://kroto.in/api/og/event?title=${
-            event?.title ?? ""
-          }&datetime=${event?.datetime?.getTime() ?? 0}&host=${
-            event?.creator?.name ?? ""
-          }`}
-        />
+        <meta name="twitter:image" content={ogImage} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <main className="-mt-10 flex h-full min-h-screen w-full flex-col items-center gap-8 overflow-x-hidden py-12">
