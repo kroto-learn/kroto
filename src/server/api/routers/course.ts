3a4da5fe21a2c7671fa3a24a30b365c1d5e9221d
Yuvraj Singh <ysgaur9919@gmail.com>
2023-05-09 17:15:14 +0530

    update: created enrollment flow

--- src/server/api/routers/course.ts
+++ src/server/api/routers/course.ts
@@ -25,6 +25,11 @@ export const courseRouter = createTRPCRouter({
           creator: true,
           courseBlockVideos: true,
           courseBlockMds: true,
+          enrollments: {
+            include: {
+              user: true,
+            },
+          },
         },
       });
 
@@ -238,22 +243,96 @@ export const courseRouter = createTRPCRouter({
   //     return event;
   //   }),
 
+  enroll: protectedProcedure
+    .input(z.object({ courseId: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { prisma } = ctx;
+
+      const course = await prisma.course.findUnique({
+        where: { id: input.courseId },
+      });
+
+      const user = await prisma.user.findUnique({
+        where: {
+          id: ctx.session.user.id,
+        },
+      });
+
+      if (!course || !user) return new TRPCError({ code: "BAD_REQUEST" });
+
+      if (course.creatorId === user.id)
+        return new TRPCError({ code: "BAD_REQUEST" });
+
+      const enrollment = await prisma.enrollment.create({
+        data: {
+          userId: user.id,
+          courseId: course.id,
+        },
+      });
+
+      const audienceMember = await prisma.audienceMember.findFirst({
+        where: {
+          email: user.email,
+          creatorId: course.creatorId,
+        },
+      });
+
+      /* Adding to audience list */
+      if (!audienceMember) {
+        // if audience member doesn't exist, create one
+        await prisma.audienceMember.create({
+          data: {
+            email: user.email,
+            name: user.name,
+            userId: user.id,
+            creatorId: course.creatorId,
+            eventId: course.id,
+          },
+        });
+      }
+
+      // const creator = await prisma.user.findUnique({
+      //   where: {
+      //     id: course.creatorId,
+      //   },
+      // });
+
+      // TODO: send course enrollment confirmation
+      // try {
+      //   await sendRegistrationConfirmation(
+      //     course,
+      //     creator,
+      //     user.email,
+      //     user.name
+      //   );
+      // } catch (e) {
+      //   console.log(e);
+      // }
+
+      return enrollment;
+    }),
+
+  isEnrolled: publicProcedure
+    .input(z.object({ courseId: z.string() }))
+    .query(({ ctx, input }) => {
+      const { prisma } = ctx;
+
+      if (!ctx.session) return null;
+
+      const enrolled = prisma.enrollment.findFirst({
+        where: {
+          userId: ctx.session.user.id,
+          courseId: input.courseId,
+        },
+      });
+
+      return enrolled;
+    }),
+
   delete: protectedProcedure
     .input(z.object({ id: z.string() }))
     .mutation(async ({ ctx, input }) => {
       const { prisma } = ctx;
-
-      await prisma.courseBlockVideo.deleteMany({
-        where: {
-          courseId: input.id,
-        },
-      });
-
-      await prisma.courseBlockMd.deleteMany({
-        where: {
-          courseId: input.id,
-        },
-      });
 
       const course = await prisma.course.delete({
         where: {
