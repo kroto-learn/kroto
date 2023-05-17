9394cd79b821426e7db67f29ad2fff1e794d0c85
rosekamallove <rosekamallove@gmail.com>
2023-05-17 16:17:29 +0530

    removed: ssg creator profile

--- src/pages/[creator_id]/index.tsx
+++ src/pages/[creator_id]/index.tsx
@@ -224,7 +224,7 @@ const Index = ({ creatorProfile }: CreatorPageProps) => {
                     <Image src="/empty/event_empty.svg" alt="empty" fill />
                   </div>
                   <p className="mb-2 text-neutral-400">
-                    The creator has not created any events.
+                    The creater has not created any events.
                   </p>
                 </div>
               )
@@ -248,16 +248,8 @@ const Index = ({ creatorProfile }: CreatorPageProps) => {
                     />
                   </div>
                   <p className="mb-2 text-neutral-400">
-                    The creator has not got any testimonials.
+                    The creater has not got any testimonials.
                   </p>
-                  <Link
-                    id="testimonial"
-                    href={`/${creator?.creatorProfile ?? ""}/testimonial`}
-                    className="group mt-2 flex w-full max-w-xs items-center justify-center gap-2 rounded-xl bg-neutral-200/10 px-4 py-2 pr-[1.2rem] text-sm font-medium text-neutral-300 backdrop-blur-sm transition-all duration-300 hover:bg-pink-500/80 hover:text-neutral-200"
-                  >
-                    <FontAwesomeIcon icon={faQuoteLeft} /> Write a Testimonial
-                    for me
-                  </Link>
                 </div>
               )
             ) : creator.courses && creator.courses.length > 0 ? (
@@ -269,10 +261,10 @@ const Index = ({ creatorProfile }: CreatorPageProps) => {
             ) : (
               <div className="flex w-full flex-col items-center justify-center gap-2 p-4">
                 <div className="relative aspect-square w-40 object-contain">
-                  <Image src="/empty/course_empty.svg" alt="empty" fill />
+                  <Image src="/empty/event_empty.svg" alt="empty" fill />
                 </div>
                 <p className="mb-2 text-neutral-400">
-                  The creator has not created any courses.
+                  The creater has not created any courses.
                 </p>
               </div>
             )}
@@ -290,28 +282,28 @@ const Index = ({ creatorProfile }: CreatorPageProps) => {
   );
 };
 
-export const getStaticPaths = () => {
-  return { paths: [], fallback: "blocking" };
-};
+// export const getStaticPaths = () => {
+//   return { paths: [], fallback: "blocking" };
+// };
 
-interface CParams extends ParsedUrlQuery {
-  creator_id: string;
-}
+// interface CParams extends ParsedUrlQuery {
+//   creator_id: string;
+// }
 
-export async function getStaticProps(context: GetStaticPropsContext) {
-  const ssg = generateSSGHelper();
-  const creatorProfile = (context.params as CParams).creator_id;
+// export async function getStaticProps(context: GetStaticPropsContext) {
+//   const ssg = generateSSGHelper();
+//   const creatorProfile = (context.params as CParams).creator_id;
 
-  if (typeof creatorProfile !== "string") throw new Error("no slug");
+//   if (typeof creatorProfile !== "string") throw new Error("no slug");
 
-  await ssg.creator.getPublicProfile.prefetch({ creatorProfile });
+//   await ssg.creator.getPublicProfile.prefetch({ creatorProfile });
 
-  return {
-    props: {
-      trpcState: ssg.dehydrate(),
-      creatorProfile,
-    },
-  };
-}
+//   return {
+//     props: {
+//       trpcState: ssg.dehydrate(),
+//       creatorProfile,
+//     },
+//   };
+// }
 
 export default Index;
