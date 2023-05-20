95e2d296490d33d77731d0da47d7b77e2a17a890
Yuvraj Singh <ysgaur9919@gmail.com>
2023-05-21 03:00:55 +0530

    update: course player ui update & next, previous buttons

--- src/pages/course/play/[course_id]/index.tsx
+++ src/pages/course/play/[course_id]/index.tsx
@@ -1,22 +1,25 @@
 import { Loader } from "@/components/Loader";
 import { api } from "@/utils/api";
 import {
-  ArrowsPointingInIcon,
-  ArrowsPointingOutIcon,
   PlayIcon,
   CheckIcon,
-  ArrowRightOnRectangleIcon,
+  ArrowLeftIcon,
+  ShareIcon,
+  ChevronRightIcon,
+  ChevronLeftIcon,
+  ListBulletIcon,
 } from "@heroicons/react/20/solid";
 import { ClockIcon } from "@heroicons/react/24/outline";
 import { type Chapter, type ChapterProgress } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
-import { Checkbox, ConfigProvider, Tooltip, theme } from "antd";
+import { Checkbox, ConfigProvider, theme } from "antd";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { type ReactNode, useEffect, useState, useRef } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip as TooltipC } from "chart.js";
 import { Pie } from "react-chartjs-2";
+import useToast from "@/hooks/useToast";
 
 ChartJS.register(ArcElement, TooltipC);
 
@@ -87,6 +90,8 @@ const PlayerLayoutR = ({ children }: { children: ReactNode }) => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  const { successToast } = useToast();
+
   if (course instanceof TRPCError || !course) return <></>;
 
   const chaptersWatched = course.chapters.filter(
@@ -117,160 +122,181 @@ const PlayerLayoutR = ({ children }: { children: ReactNode }) => {
     radius: sideDrawerCollapsed ? 25 : 40,
   };
 
+  const courseUrl = `https://kroto.in/course/${course?.id ?? ""}`;
+
   return (
-    <div className="flex min-h-screen w-full flex-col-reverse justify-end gap-3 p-4 sm:flex-row sm:justify-start">
-      {children}
-      <div
-        className={`flex rounded-lg border border-neutral-700 bg-neutral-950/80 backdrop-blur-sm sm:bg-neutral-200/5 ${
-          sideDrawerCollapsed
-            ? "right-4 top-4 flex-row-reverse sm:sticky sm:h-[calc(100vh-2rem)] sm:max-w-[5rem] sm:flex-col"
-            : "fixed right-4 top-4 h-[calc(100vh-2rem)] w-4/5 max-w-sm flex-col sm:sticky sm:w-full"
-        }`}
-      >
+    <div className="flex min-h-screen w-full flex-col">
+      <div className="sticky top-0 z-10 flex w-full justify-between gap-4 bg-gradient-to-b from-neutral-950 via-neutral-950/70 to-transparent p-4 text-neutral-400 duration-150 hover:text-neutral-300">
+        <Link
+          href="/dashboard"
+          className="flex items-center gap-1 text-xs font-bold uppercase tracking-wider sm:text-sm"
+        >
+          <ArrowLeftIcon className="w-4" /> Back to dashboard
+        </Link>
+        <button
+          onClick={() => {
+            void navigator.clipboard.writeText(courseUrl);
+            successToast("Course link copied to clipboard!");
+          }}
+          className="flex items-center gap-1 text-xs font-bold uppercase tracking-wider sm:text-sm"
+        >
+          <ShareIcon className="w-3" /> Share Course
+        </button>
+      </div>
+      <div className="flex w-full  flex-col-reverse justify-end gap-6 p-4 pt-3 sm:flex-row sm:justify-start">
+        {children}
         <div
-          className={`flex flex-col gap-2 border-neutral-700 ${
+          className={`flex rounded-lg backdrop-blur-sm sm:bg-neutral-200/5 ${
             sideDrawerCollapsed
-              ? "border-l p-1 sm:w-full sm:border-b sm:border-l-0"
-              : "w-full border-b p-4 px-6"
+              ? "right-4 top-16 flex-row-reverse border-neutral-700 sm:sticky sm:h-[calc(100vh-5rem)] sm:max-w-[5rem] sm:flex-col sm:border sm:bg-neutral-950/80"
+              : "fixed right-4 top-16 h-[calc(100vh-5rem)] w-4/5 max-w-sm flex-col border border-neutral-700 bg-neutral-950/80 sm:sticky sm:w-full"
           }`}
         >
-          <div
-            className={`flex items-start justify-between ${
-              sideDrawerCollapsed ? "flex-col gap-1 sm:flex-row" : "gap-2"
+          <button
+            className={`absolute -left-5 top-6 z-10 aspect-square rounded-full border border-neutral-600 bg-neutral-900 p-2 text-neutral-400 drop-shadow-xl duration-150 hover:bg-neutral-800 hover:text-neutral-300 ${
+              sideDrawerCollapsed ? "hidden sm:flex" : ""
             }`}
+            onClick={() => setSideDrawerCollapsed(!sideDrawerCollapsed)}
           >
             {sideDrawerCollapsed ? (
-              <></>
+              <ChevronLeftIcon className="w-5" />
             ) : (
-              <Link
-                href={`/course/${course?.id ?? ""}`}
-                className="line-clamp-2 overflow-hidden text-ellipsis text-lg font-medium duration-150 hover:text-neutral-100"
-              >
-                {course?.title}
-              </Link>
+              <ChevronRightIcon className="w-5" />
             )}
-            <button
-              onClick={() => setSideDrawerCollapsed(!sideDrawerCollapsed)}
-              className={`aspect-square rounded-full p-2 text-neutral-400 duration-150 hover:bg-neutral-700 hover:text-neutral-300 ${
-                sideDrawerCollapsed ? "" : "bg-neutral-800"
+          </button>
+          <div
+            className={`flex w-full flex-col gap-2 border-b border-neutral-700 p-4 px-6 transition-all duration-300 ${
+              sideDrawerCollapsed ? "hidden" : ""
+            }`}
+          >
+            <div
+              className={`flex items-start justify-between ${
+                sideDrawerCollapsed ? "flex-col gap-1 sm:flex-row" : "gap-2"
               }`}
             >
               {sideDrawerCollapsed ? (
-                <ArrowsPointingOutIcon className="w-4" />
+                <></>
               ) : (
-                <ArrowsPointingInIcon className="w-4" />
+                <Link
+                  href={`/course/${course?.id ?? ""}`}
+                  className="line-clamp-2 overflow-hidden text-ellipsis text-lg font-medium duration-150 hover:text-neutral-100"
+                >
+                  {course?.title}
+                </Link>
               )}
-            </button>
-            <Tooltip title="Exit Course Player">
-              <Link
-                href="/dashboard"
-                className={`aspect-square rounded-full p-2 text-neutral-400 duration-150 hover:bg-red-500/30 hover:text-neutral-300  ${
-                  sideDrawerCollapsed ? "" : "bg-neutral-800"
-                }`}
-              >
-                <ArrowRightOnRectangleIcon className="w-4" />
-              </Link>
-            </Tooltip>
-          </div>
-
-          {sideDrawerCollapsed ? (
-            <></>
-          ) : (
-            <div className="flex items-center gap-2 text-sm text-neutral-300">
-              <Link
-                href={`/${course?.creator?.creatorProfile ?? ""}`}
-                className="duration-150 hover:text-neutral-200"
-              >
-                {course?.creator?.name}
-              </Link>{" "}
-              • <p>{course?.chapters?.length} Chapters</p>
             </div>
-          )}
-        </div>
-        <div
-          className={`flex items-center gap-4 overflow-hidden border-neutral-700 p-1 sm:p-2 [&.chartjs-legend]:hidden ${
-            sideDrawerCollapsed
-              ? "border-l sm:w-full sm:border-b sm:border-l-0"
-              : "w-full border-b"
-          }`}
-        >
+
+            {sideDrawerCollapsed ? (
+              <></>
+            ) : (
+              <div className="flex items-center gap-2 text-sm text-neutral-300">
+                <Link
+                  href={`/${course?.creator?.creatorProfile ?? ""}`}
+                  className="duration-150 hover:text-neutral-200"
+                >
+                  {course?.creator?.name}
+                </Link>{" "}
+                • <p>{course?.chapters?.length} Chapters</p>
+              </div>
+            )}
+          </div>
+          {
+            <button
+              className={`${sideDrawerCollapsed ? "px-2 sm:hidden" : "hidden"}`}
+              onClick={() => setSideDrawerCollapsed(!sideDrawerCollapsed)}
+            >
+              <ListBulletIcon className="w-6 text-pink-500" />
+            </button>
+          }
           <div
-            className={`flex items-center justify-center ${
+            className={`flex items-center overflow-hidden border-neutral-700 p-1 sm:p-2 [&.chartjs-legend]:hidden ${
               sideDrawerCollapsed
-                ? "hidden h-16 w-16 sm:flex"
-                : "h-24 w-24 sm:h-24 sm:w-24"
+                ? "gap-2 sm:w-full sm:gap-4 sm:border-b sm:border-l-0"
+                : "w-full gap-2 border-b sm:gap-4"
             }`}
           >
-            <Pie data={data} options={options} />
-          </div>
-          <h5
-            className={`${sideDrawerCollapsed ? "block sm:hidden " : "hidden"}`}
-          >
-            <span className="m-0 p-0 text-xl font-bold text-green-600">
-              {Math.ceil((chaptersWatched / course.chapters?.length) * 100)}
-              <span className="m-0 p-0 text-sm font-normal leading-3">%</span>
-            </span>
-          </h5>
-
-          {sideDrawerCollapsed ? (
-            <></>
-          ) : (
-            <div className="grid grid-cols-2 gap-5">
-              <div className="m-0 flex items-end p-0 text-xs leading-3 text-neutral-300">
-                <span className="m-0 mr-1 p-0 text-xl font-bold leading-3 text-green-600">
-                  {Math.ceil((chaptersWatched / course.chapters?.length) * 100)}
-                  <span className="m-0 p-0 text-sm font-normal leading-3">
-                    %
-                  </span>
-                </span>{" "}
-                completed
-              </div>
-              <div className="m-0 flex items-center p-0 text-xs leading-3 text-neutral-300">
-                <span className="m-0 mr-1 p-0 text-xl font-bold leading-3 text-neutral-200">
-                  {minutesWatched}
-                  <span className="m-0 p-0 text-sm font-normal leading-3">
-                    mins
-                  </span>
-                </span>{" "}
-                watched
-              </div>
-              <div className="m-0 flex items-end p-0 text-xs leading-3 text-neutral-300">
-                <span className="m-0 mr-1 p-0 text-xl font-bold leading-3 text-green-600">
-                  {chaptersWatched}
-                  <span className="m-0 p-0 text-sm font-normal leading-3">
-                    chs
-                  </span>
-                </span>{" "}
-                watched
-              </div>
-              <div className="m-0 flex items-end p-0 text-xs leading-3 text-neutral-300">
-                <span className="m-0 mr-1 p-0 text-xl font-bold leading-3 text-neutral-400">
-                  {course.chapters?.length - chaptersWatched}
-                  <span className="m-0 p-0 text-sm font-normal leading-3">
-                    chs
-                  </span>
-                </span>{" "}
-                remaining
-              </div>
+            <div
+              className={`flex items-center justify-center ${
+                sideDrawerCollapsed
+                  ? "hidden h-16 w-16 sm:flex"
+                  : "h-24 w-24 sm:h-24 sm:w-24"
+              }`}
+            >
+              <Pie data={data} options={options} />
             </div>
-          )}
-        </div>
-        <div
-          ref={chaptersNavRef}
-          className={`flex w-full justify-start overflow-auto  ${
-            sideDrawerCollapsed
-              ? "hide-scroll flex-row items-center sm:max-h-[calc(100vh-8rem)] sm:flex-col"
-              : "max-h-[calc(100vh-8rem)] flex-col"
-          }`}
-        >
-          {course?.chapters?.map((chapter, idx) => (
-            <CoursePlayerChapterTile
-              collapsed={sideDrawerCollapsed}
-              idx={idx}
-              chapter={chapter}
-              key={chapter?.id}
-            />
-          ))}
+            <h5
+              className={`${
+                sideDrawerCollapsed ? "block px-2 pr-3 sm:hidden" : "hidden"
+              }`}
+            >
+              <span className="m-0 p-0 text-xl font-bold text-green-600">
+                {Math.ceil((chaptersWatched / course.chapters?.length) * 100)}
+                <span className="m-0 p-0 text-sm font-normal leading-3">%</span>
+              </span>
+            </h5>
+
+            {sideDrawerCollapsed ? (
+              <></>
+            ) : (
+              <div className="grid grid-cols-2 gap-x-1 gap-y-5 sm:gap-5">
+                <div className="m-0 flex items-end p-0 text-xs leading-3 text-neutral-300">
+                  <span className="m-0 mr-1 p-0 text-xl font-bold leading-3 text-green-600">
+                    {Math.ceil(
+                      (chaptersWatched / course.chapters?.length) * 100
+                    )}
+                    <span className="m-0 p-0 text-sm font-normal leading-3">
+                      %
+                    </span>
+                  </span>{" "}
+                  completed
+                </div>
+                <div className="m-0 flex items-center p-0 text-xs leading-3 text-neutral-300">
+                  <span className="m-0 mr-1 p-0 text-xl font-bold leading-3 text-neutral-200">
+                    {minutesWatched}
+                    <span className="m-0 p-0 text-sm font-normal leading-3">
+                      mins
+                    </span>
+                  </span>{" "}
+                  watched
+                </div>
+                <div className="m-0 flex items-end p-0 text-xs leading-3 text-neutral-300">
+                  <span className="m-0 mr-1 p-0 text-xl font-bold leading-3 text-green-600">
+                    {chaptersWatched}
+                    <span className="m-0 p-0 text-sm font-normal leading-3">
+                      chs
+                    </span>
+                  </span>{" "}
+                  watched
+                </div>
+                <div className="m-0 flex items-end p-0 text-xs leading-3 text-neutral-300">
+                  <span className="m-0 mr-1 p-0 text-xl font-bold leading-3 text-neutral-400">
+                    {course.chapters?.length - chaptersWatched}
+                    <span className="m-0 p-0 text-sm font-normal leading-3">
+                      chs
+                    </span>
+                  </span>{" "}
+                  remaining
+                </div>
+              </div>
+            )}
+          </div>
+          <div
+            ref={chaptersNavRef}
+            className={`flex w-full justify-start overflow-auto  ${
+              sideDrawerCollapsed
+                ? "hide-scroll flex-row items-center justify-start sm:max-h-[calc(100vh-16rem)] sm:flex-col"
+                : "max-h-[calc(100vh-16rem)] flex-col"
+            }`}
+          >
+            {course?.chapters?.map((chapter, idx) => (
+              <CoursePlayerChapterTile
+                collapsed={sideDrawerCollapsed}
+                idx={idx}
+                chapter={chapter}
+                key={chapter?.id}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
@@ -309,16 +335,16 @@ const CoursePlayerChapterTile = ({ chapter, idx, collapsed }: CPCTProps) => {
     <Link
       href={`/course/play/${chapter?.courseId}/${chapter?.id}`}
       id={`${chapter?.id}`}
-      className={`flex w-full max-w-lg items-center border-neutral-700 ${
+      className={`flex items-center border-neutral-700 ${
         !(chapter.id === chapter_id)
           ? chapter.chapterProgress
             ? "!bg-green-950/30 hover:!bg-green-800/30"
             : "hover:bg-neutral-200/10"
           : " !bg-pink-900/10 hover:!bg-pink-900/20"
-      }  bg-transparent p-2 px-4 backdrop-blur-sm duration-150 ${
+      }  bg-transparent backdrop-blur-sm duration-150 ${
         collapsed
-          ? "aspect-square gap-2 border-r sm:border-b sm:border-r-0"
-          : "gap-3 border-b"
+          ? "mx-1 h-12 gap-2 rounded-lg border p-1 px-2 sm:m-0 sm:aspect-square sm:h-auto sm:w-full sm:max-w-lg sm:rounded-none sm:border-0 sm:border-b sm:border-r-0 sm:p-2 sm:px-4"
+          : "w-full max-w-lg gap-3 border-b p-2 px-4"
       }`}
       key={chapter?.id}
     >
@@ -344,6 +370,7 @@ const CoursePlayerChapterTile = ({ chapter, idx, collapsed }: CPCTProps) => {
                 {
                   onSuccess: () => {
                     void ctx.course.get.invalidate();
+                    void ctx.courseChapter.get.invalidate();
                   },
                 }
               );
@@ -355,6 +382,7 @@ const CoursePlayerChapterTile = ({ chapter, idx, collapsed }: CPCTProps) => {
                 {
                   onSuccess: () => {
                     void ctx.course.get.invalidate();
+                    void ctx.courseChapter.get.invalidate();
                   },
                 }
               );
@@ -364,11 +392,13 @@ const CoursePlayerChapterTile = ({ chapter, idx, collapsed }: CPCTProps) => {
 
       <p className={`text-xs text-neutral-300`}>
         {chapter_id === chapter?.id ? (
-          <PlayIcon className={`text-pink-500 ${collapsed ? "w-4" : "w-3"}`} />
+          <PlayIcon
+            className={`text-pink-500 ${collapsed ? "w-5" : "w-5 pr-2"}`}
+          />
         ) : (
           <div
             className={`aspect-square ${
-              collapsed ? "w-4 text-xl font-medium" : "w-3"
+              collapsed ? "w-5 text-xl font-medium" : "w-5"
             }`}
           >
             {collapsed ? (
@@ -398,7 +428,7 @@ const CoursePlayerChapterTile = ({ chapter, idx, collapsed }: CPCTProps) => {
             className={`flex h-full w-full flex-col items-start justify-between gap-1`}
           >
             <h5
-              className={`line-clamp-2 overflow-hidden text-ellipsis text-xs font-medium`}
+              className={`line-clamp-2 overflow-hidden text-ellipsis text-xs font-semibold`}
             >
               {chapter?.title}
             </h5>
