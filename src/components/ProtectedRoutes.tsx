64e33a9a60a5634228b2a336cde488332c19d9ff
rosekamallove <rosekamallove@gmail.com>
2023-05-21 20:22:56 +0530

    added: cold start loading ?

--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -1,16 +1,22 @@
+import ColdLoading from "@/pages/cold-loading";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 
 const ProtectedRoutes = () => {
   const router = useRouter();
-
   const { data: session, status } = useSession();
 
   useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (status === "loading" && router.asPath !== "/")
+        void router.push(`/cold-loading?path=${router.asPath}`);
+    }, 2000);
+
     const publicRoutes = [
       "/",
       "/privacy",
+      "/cold-loading",
       "/terms-of-service",
       "/terms-and-conditions",
       "/auth/sign-in",
@@ -24,6 +30,10 @@ const ProtectedRoutes = () => {
     if (status === "unauthenticated") {
       void router.push("/");
     }
+
+    return () => {
+      clearTimeout(timeout);
+    };
   }, [status, router, session]);
 
   return <></>;
