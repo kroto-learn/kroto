20e4e5f7b8b2ae8f2dfbbf2f988ce73fdf2fd378
anuprajvarma <anupraj1854@gmail.com>
2023-06-18 19:49:14 +0530

    make askedQuery routes

--- src/server/api/routers/creator/creator.ts
+++ src/server/api/routers/creator/creator.ts
@@ -69,8 +69,13 @@ export const creatorRouter = createTRPCRouter({
         where: { creatorProfile },
         include: { user: true },
       });
+      
+      const querys = await prisma.askedQuery.findMany({
+        where: { creatorProfile },
+        include: { user: true },
+      });
 
-      return { ...creator, testimonials };
+      return { ...creator, testimonials, querys };
     }),
 
   getProfile: protectedProcedure.query(async ({ ctx }) => {
