1047789da2d304a8fdc77e6fe5a0de30d710fc63
anuprajvarma <anupraj1854@gmail.com>
2023-07-03 23:43:37 +0530

    style contact-us page

--- src/pages/contact-us/index.tsx
+++ src/pages/contact-us/index.tsx
@@ -43,116 +43,105 @@ export default function Contact() {
       <Head>
         <title>Contact</title>
       </Head>
-      <div className="text-center my-10">
+      <div className="px-2 py-10 text-center">
         <h1 className="text-5xl font-semibold">Contact Us</h1>
         <p className="mt-2 text-3xl">Ask any query or contact our team</p>
       </div>
-        <div className="w-full">
-            <form
-              onSubmit={methods.handleSubmit((values) => {
-                void contactMutation(values, {
-                  onSuccess: () => {
-                    successToast(
-                      "We have receive your message! We will contact you shortly."
-                    );
-                    methods.setValue("name", "");
-                    methods.setValue("email", "");
-                    methods.setValue("phone", "");
-                    methods.setValue("message", "");
-                  },
-                });
-              })}
-              className="flex flex-col items-center"
+      <div className="flex w-full items-center justify-center">
+        <form
+          onSubmit={methods.handleSubmit((values) => {
+            void contactMutation(values, {
+              onSuccess: () => {
+                successToast(
+                  "We have receive your message! We will contact you shortly."
+                );
+                methods.setValue("name", "");
+                methods.setValue("email", "");
+                methods.setValue("phone", "");
+                methods.setValue("message", "");
+              },
+            });
+          })}
+          className="flex w-4/6 flex-col md:w-3/6 lg:w-2/5"
+        >
+          <div className="mb-2">
+            <label className="mb-2 block font-medium text-neutral-400">
+              Your Name <span className="text-red-700">*</span>
+            </label>
+            <input
+              {...methods.register("name")}
+              className="w-full rounded-xl border border-neutral-700 bg-neutral-800 px-3 py-2 placeholder-neutral-400 outline-none ring-transparent transition duration-300 hover:border-neutral-500 focus:border-neutral-400 focus:ring-neutral-500 active:outline-none active:ring-transparent"
+              placeholder="Enter Your Name"
+            />
+            {methods.formState.errors.name?.message && (
+              <p className="text-red-700">
+                {methods.formState.errors.name?.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-2">
+            <label className="mb-2 block font-medium text-neutral-400">
+              Your Email <span className="text-red-700">*</span>
+            </label>
+            <input
+              {...methods.register("email")}
+              className="block w-full rounded-xl border border-neutral-700 bg-neutral-800 px-3 py-2 placeholder-neutral-400 outline-none ring-transparent transition duration-300 hover:border-neutral-500 focus:border-neutral-400 focus:ring-neutral-500 active:outline-none active:ring-transparent"
+              placeholder="Enter your Email"
+            />
+            {methods.formState.errors.email?.message && (
+              <p className="text-red-700">
+                {methods.formState.errors.email?.message}
+              </p>
+            )}
+          </div>
+          <div className="mb-2">
+            <label className="mb-2 block font-medium text-neutral-400">
+              Your Phone Number
+            </label>
+            <input
+              {...methods.register("phone")}
+              className="block w-full rounded-xl border border-neutral-700 bg-neutral-800 px-3 py-2 placeholder-neutral-400 outline-none ring-transparent transition duration-300 hover:border-neutral-500 focus:border-neutral-400 focus:ring-neutral-500 active:outline-none active:ring-transparent"
+              placeholder="Enter your Phone Number"
+            />
+          </div>
+          <div className="mb-2">
+            <label className="mb-2 block font-medium text-neutral-400">
+              Message <span className="text-red-700">*</span>
+            </label>
+            <textarea
+              value={methods.watch().message}
+              onChange={(e) => {
+                methods.setValue("message", e.target?.value.substring(0, 500));
+              }}
+              className="block w-full rounded-xl border border-neutral-700 bg-neutral-800 px-3 py-2 placeholder-neutral-400 outline-none ring-transparent transition duration-300 hover:border-neutral-500 focus:border-neutral-400 focus:ring-neutral-500 active:outline-none active:ring-transparent"
+              placeholder="Enter Your Message"
+            />
+            {
+              <p className="text-end text-neutral-400">
+                {methods.watch()?.message?.length ?? 0}/{500}
+              </p>
+            }
+            {methods.formState.errors.message?.message && (
+              <p className="text-red-700">
+                {methods.formState.errors.message?.message}
+              </p>
+            )}
+          </div>
+          <div className="w-full">
+            <button
+              type="submit"
+              className={`group flex w-full items-center justify-center gap-1 rounded-xl bg-pink-600 px-3 py-2 font-semibold text-white transition-all duration-300 hover:bg-pink-700 sm:w-auto`}
             >
-              <div className="mb-2 w-1/4">
-                <label className="mb-2 block font-medium text-neutral-400">
-                  Your Name <span className="text-red-700">*</span>
-                </label>
-                <div>
-                  <input
-                    {...methods.register("name")}
-                    className="block w-full rounded-xl border border-neutral-700 bg-neutral-800 px-3 py-2 placeholder-neutral-400 outline-none ring-transparent transition duration-300 hover:border-neutral-500 focus:border-neutral-400 focus:ring-neutral-500 active:outline-none active:ring-transparent"
-                    placeholder="Enter Your Name"
-                  />
-                </div>
-                {methods.formState.errors.name?.message && (
-                  <p className="text-red-700">
-                    {methods.formState.errors.name?.message}
-                  </p>
-                )}
-              </div>
-              <div className="mb-2 w-1/4">
-                <label className="mb-2 block font-medium text-neutral-400">
-                  Your Email <span className="text-red-700">*</span>
-                </label>
-                <div className="">
-                  <input
-                    {...methods.register("email")}
-                    className="block w-full rounded-xl border border-neutral-700 bg-neutral-800 px-3 py-2 placeholder-neutral-400 outline-none ring-transparent transition duration-300 hover:border-neutral-500 focus:border-neutral-400 focus:ring-neutral-500 active:outline-none active:ring-transparent"
-                    placeholder="Enter your Email"
-                  />
-                </div>
-                {methods.formState.errors.email?.message && (
-                  <p className="text-red-700">
-                    {methods.formState.errors.email?.message}
-                  </p>
-                )}
-              </div>
-              <div className="mb-2 w-1/4">
-                <label className="mb-2 block font-medium text-neutral-400">
-                  Your Phone Number
-                </label>
-                <div>
-                  <input
-                    {...methods.register("phone")}
-                    className="block w-full rounded-xl border border-neutral-700 bg-neutral-800 px-3 py-2 placeholder-neutral-400 outline-none ring-transparent transition duration-300 hover:border-neutral-500 focus:border-neutral-400 focus:ring-neutral-500 active:outline-none active:ring-transparent"
-                    placeholder="Enter your Phone Number"
-                  />
-                </div>
-              </div>
-              <div className="mb-2 w-1/4">
-                <label className="mb-2 block font-medium text-neutral-400">
-                  Message <span className="text-red-700">*</span>
-                </label>
-                <div>
-                  <textarea
-                    value={methods.watch().message}
-                    onChange={(e) => {
-                      methods.setValue(
-                        "message",
-                        e.target?.value.substring(0, 500)
-                      );
-                    }}
-                    className="block w-full rounded-xl border border-neutral-700 bg-neutral-800 px-3 py-2 placeholder-neutral-400 outline-none ring-transparent transition duration-300 hover:border-neutral-500 focus:border-neutral-400 focus:ring-neutral-500 active:outline-none active:ring-transparent"
-                    placeholder="Enter Your Message"
-                  />
-                </div>
-                {
-                  <p className="text-end text-neutral-400">
-                    {methods.watch()?.message?.length ?? 0}/{500}
-                  </p>
-                }
-                {methods.formState.errors.message?.message && (
-                  <p className="text-red-700">
-                    {methods.formState.errors.message?.message}
-                  </p>
-                )}
-              </div>
-              <div className="w-1/4">
-              <button
-                type="submit"
-                className={` group inline-flex gap-1 rounded-xl bg-pink-600 px-6 py-2 text-center font-medium text-white transition-all duration-300 hover:bg-pink-700 `}
-              >
-                Send Message{" "}
-                {contactMutationLoading ? (
-                  <Loader white />
-                ) : (
-                  <PaperAirplaneIcon className="w-4" />
-                )}
-              </button>
-              </div>
-            </form>
-        </div>
+              Send Message{" "}
+              {contactMutationLoading ? (
+                <Loader white />
+              ) : (
+                <PaperAirplaneIcon className="w-4 pt-1" />
+              )}
+            </button>
+          </div>
+        </form>
+      </div>
     </Layout>
   );
 }
